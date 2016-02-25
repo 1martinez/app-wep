8351414faa8e5a104687e0911d1f59aecfb5c0e5
yuliñoelman <tumartinez12@gmail.com>
2016-02-25 15:36:19 -0500

    tu pai mas na jejeje xd

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>todos los dias a estudiar</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee>
+<h1>cada dia siempre estas en mis sueños no puedo olvidarte</h1>
+</marquee>	
+
+</body>
+</html>
